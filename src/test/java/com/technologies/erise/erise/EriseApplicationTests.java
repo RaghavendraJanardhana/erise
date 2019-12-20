e26abe21109ee9a42606ca4ddaffc68d34996d18
Raghavendra J <Raghavendra.janardhana@tecnotree.com>
2019-12-20 16:37:56 +0530

    initial commit.

---
+++ src/test/java/com/technologies/erise/erise/EriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technologies.erise.erise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
